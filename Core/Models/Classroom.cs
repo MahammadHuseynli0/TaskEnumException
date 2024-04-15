40e761d72e4a0d8adb853172be8450439512f0fc
MahammadHuseynli0 <tu7uv5kks.@code.edu.az>
2024-04-15 11:44:29 +0400

    uploaded

---
+++ Core/Models/Classroom.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Enums;
+using Core.Exceptions;
+
+namespace Core.Models
+{
+    public class Classroom
+    {
+        private static int _id;
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Limit { get; set; }
+
+        public Student[] students = new Student[] {};
+        public Classroom[] Classrooms = new Classroom[] {};
+
+        public Core.Enums.Type ClassType { get; set; }
+
+        public Classroom()
+        {
+            
+        }
+
+
+
+        public Classroom(string classroomname, byte classType)
+        {
+            _id++;
+            Id = _id;
+            Name = classroomname;
+
+            if (classType == 1)
+            {
+                ClassType = Enums.Type.Frontend;
+                Limit = 15;
+
+            }
+
+            else if (classType == 2)
+            {
+                ClassType = Enums.Type.Frontend;
+                Limit = 20;
+            }
+
+
+        }
+
+        public void AddStudent(int no ,Student student)
+        {
+            bool ClassFound = false;
+            for (int i = 0; i < Classrooms.Length; i++)
+            {
+
+                if (Classrooms[i].Id == no)
+                {
+                    ClassFound = true;
+                    Array.Resize(ref Classrooms[i].students, Classrooms[i].students.Length + 1);
+                    Classrooms[i].students[Classrooms[i].students.Length - 1] = student;
+                }
+            }
+            
+            if (ClassFound == false)
+            {
+                throw new ClassroomNotFoundException("Classroom tapilmadi!");
+            }
+
+            
+        }
+
+        public void AddClassroom(Classroom classroom)
+        {
+            Array.Resize(ref Classrooms, Classrooms.Length + 1);
+            Classrooms[Classrooms.Length - 1] = classroom;
+        }
+
+
+
+        public void ShowClassrooms()
+        {
+            for (int i = 0; i < Classrooms.Length; i++)
+            {
+                Console.WriteLine($"{Classrooms[i].Id} {Classrooms[i].Name}");
+            }
+        }
+
+        public void ShowAllStudents()
+        {
+            for (int i = 0; i < Classrooms.Length; i++)
+            {
+                Console.WriteLine($"{Classrooms[i].Id} {Classrooms[i].Name} - : \n");
+
+
+                for (int j = 0; j < Classrooms[i].students.Length; j++)
+                {
+                    Console.WriteLine($"{Classrooms[i].students[j].Id}    {Classrooms[i].students[j].Name}     {Classrooms[i].students[j].Surname}");
+                }
+            }
+        }
+
+
+
+        public void ShowStudentsForClass(int no)
+        {
+            for (int i = 0; i < Classrooms.Length; i++)
+            {
+                if (Classrooms[i].Id == no)
+                {
+                    Console.WriteLine($"Classroomdaki telebeler:");
+                   
+                    for (int j = 0; j < Classrooms[i].students.Length; j++)
+                    {
+                        Console.WriteLine($"{Classrooms[i].students[j].Id}    {Classrooms[i].students[j].Name}     {Classrooms[i].students[j].Surname}");
+                    }
+                }
+            }        
+        }
+
+
+
+
+        public Student FindId(int id)
+        {
+            
+            for(int i = 0; i < students.Length; i++)
+            {
+                if (students[i].Id == id)
+                {
+                    return students[i];
+                }
+            }
+            return null;
+        }
+         
+        public void Delete(int no)
+        {
+            bool check = false;
+
+            Student[] deletestudents = new Student[] { };
+
+            for(int i = 0;i < students.Length;i++)
+            {
+
+                if (students[i].Id == no)
+                {
+                    check = true;
+                }
+
+                else if (students[i].Id! == no)
+                {
+                    Array.Resize(ref deletestudents, deletestudents.Length+1);
+                    deletestudents[deletestudents.Length-1] = students[i];
+                }
+            }
+            students = deletestudents;
+
+            if (check == false)
+            {
+                throw new StudentNotException("Bele telebe yoxdur!");
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ TaskEnumException/Program.cs
@@ -0,0 +1,136 @@
+﻿using Core.Models;
+using Core.Enums;
+using Core.Exceptions;
+
+namespace TaskEnumException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int no;
+            int limit = 0;
+            string choice;
+            string classroomName;
+            string studentName;
+            string studentSurname;
+
+
+
+            Classroom classroom = new Classroom();
+
+            do
+            {
+                Console.WriteLine("Menu:1.Classroom yarat\n2.Student yarat\n3.Butun telebeleri ekranda goster" +
+                "\n4.Secilmis sinifdeki telebeleri ekrana cixart\n5.Telebe sil");
+                choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+
+                    do
+                    {
+                        Console.WriteLine("Classroom adini daxil edin ");
+                        classroomName = Console.ReadLine();
+                    } while (!classroomName.CheckClassroomName());
+
+
+
+
+
+                    Console.WriteLine("Classroom tipini secin:1.Frontend\n2.Backend");
+                    byte classType = Convert.ToByte(Console.ReadLine());
+
+
+                    Classroom classroom1 = new Classroom(classroomName, classType);
+
+                    classroom.AddClassroom(classroom1);
+
+                }
+
+
+
+                else if (choice == "2")
+                {
+                    do
+                    {
+                        Console.WriteLine("Student adini daxil edin: ");
+                        studentName = Console.ReadLine();
+                    } while (!studentName.CheckName());
+
+                    do
+                    {
+                        Console.WriteLine("Student soyadini daxil edin: ");
+                        studentSurname = Console.ReadLine();
+                    } while (!studentSurname.CheckSurname());
+
+                    try
+                    {
+                        if (classroom.Classrooms.Length == 0)
+                        {
+                            throw new ClassroomNotFoundException("Classroom movcud deyil!");
+                        }
+                    }
+                    catch (ClassroomNotFoundException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+
+                    }
+
+                    classroom.ShowClassrooms();
+
+                    Console.WriteLine("Student yuxaridaki qruplardan hansina elave olunacaq : (no daxil et)");
+                    no = Convert.ToInt32(Console.ReadLine());
+
+                    Student student = new Student(studentName, studentSurname);
+
+                    classroom.AddStudent(no, student);
+
+
+                }
+
+                else if (choice == "3")
+                {
+                    classroom.ShowAllStudents();
+                }
+                 
+
+                else if (choice == "4")
+                {
+                    classroom.ShowClassrooms();
+                    Console.WriteLine(" Bu qruplar var");
+                    Console.WriteLine(" Qrup secin :");
+                    no = Convert.ToInt32(Console.ReadLine());
+
+                    classroom.ShowStudentsForClass(no);
+                }
+
+
+
+
+                else if (choice == "5")
+                {
+                    
+                    Console.WriteLine("Silinen telebenin id daxil et:");                 
+                    no = Convert.ToInt32(Console.ReadLine());
+
+                    try
+                    {
+                        classroom.Delete(no);
+                    }
+                          
+                    catch(StudentNotException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+            } while (true);
+
+
+        }
+    }
+}
+
+
